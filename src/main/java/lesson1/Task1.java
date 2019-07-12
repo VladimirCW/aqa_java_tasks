fd64227e1fb849f9bac5179c8a23371a3cfccb1f
Sirenkoa <sandrew@online.ua>
2019-07-12 16:47:19 +0300

    fun min

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -5,10 +5,14 @@ public class Task1 {
     Написать функцию, которая вычисляет минимум из трёх чисел.
     */
     public static void main(String[] args) {
-        //System.out.println(min(5,2,3));
+        System.out.println(min(5,6,3));
     }
 
     public static int min(int a, int b, int c) {
+        if(a<b&&a<c) return a;
+        if(b<a&&b<c) return b;
+        if(c<b&&c<a) return c;
         return 0;
     }
+
 }
