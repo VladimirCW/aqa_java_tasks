eb5e380550863da63123a2bb8c8285ebb92da9e1
Александр Арбузов <a.arbuzov@ukr.net>
2020-05-12 12:43:03 +0300

    Task1 updated with new function

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -6,9 +6,33 @@ public class Task1 {
     */
     public static void main(String[] args) {
         //System.out.println(min(5,2,3));
+        //System.out.println(min(5,2,3));
+        try {
+            System.out.println(min(5,2,3));    // try to define lesser
+        } catch (Exception e){
+            System.out.println("Something horrible happened : " + e.getLocalizedMessage());// мало ли...
+        }
     }
 
     public static int min(int a, int b, int c) {
-        return 0;
+
+        int min = 0;
+
+        if (a < b && a < c){        //all different
+            min = a;
+        } else if (b < a & b < c) { //all different
+            min = b;
+        } else if ( c < a & c < b) { //all different
+            min = c;
+        } else if (a == b & a < c) { // some equals
+            min = a;
+        } else if (a == c & a < b) { // some equals
+            min = a;
+        } else if (b == c & a > b) {// some equals
+            min = b;
+        } else if (a == b & a == c){ // equals
+            min = a;
+        }
+        return min;
     }
 }
