8f9c4d14296a8fe3908f768794edb2219dd2b7df
Александр Арбузов <a.arbuzov@ukr.net>
2020-05-12 17:34:44 +0300

    Task1 added definerSmallerNumber Test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,15 @@
+import lesson1.Task1;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class FirstTest {
     @Test
-    public static void sucLogin(){
-        System.out.println("Passed");
+    public static void definerSmallerNumber () {
+        Task1 task1 = new Task1();
+        int min = task1.min(2,5,8);
+        int expectedMin = 1;
+        int actualMin = task1.min(2,5,8);
+        //assertEquals(actualMin, expectedMin, "Error in Task1 : Expected '" + expectedMin + "', but found '" +actualMin+ "'");
+        assertEquals(actualMin, expectedMin, "Error in Task1 : ");
     }
 }
