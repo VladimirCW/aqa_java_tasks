86b21e0aadffd98fa357b4c99a900d0a4f19639d
Александр Арбузов <a.arbuzov@ukr.net>
2020-05-12 18:56:41 +0300

    Task1 test added. Dataprovider : getIntegerData

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,15 +1,45 @@
 import lesson1.Task1;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class FirstTest {
-    @Test
-    public static void definerSmallerNumber () {
+
+    @Test(dataProvider = "getIntegerData")
+    public static void definerSmallerNumber (String sI, int expectedMin) {
+        // debug message
+        System.out.println("Instance DataProvider : Data(provided set of numbers :  "+ sI + ", expected smaller number : " + expectedMin + ")");
+
+        // get three integer for task1.min method parameters
+        String[] intChunks = sI.split(",");
+
+        // new Object here
         Task1 task1 = new Task1();
-        int min = task1.min(2,5,8);
-        int expectedMin = 1;
-        int actualMin = task1.min(2,5,8);
-        //assertEquals(actualMin, expectedMin, "Error in Task1 : Expected '" + expectedMin + "', but found '" +actualMin+ "'");
-        assertEquals(actualMin, expectedMin, "Error in Task1 : ");
+
+        // define actual min
+        int actualMin = task1.min(Integer.parseInt(intChunks[0]), Integer.parseInt(intChunks[1]), Integer.parseInt(intChunks[2]));
+
+        // assert. Please comment line below if you need a failed test
+        assertEquals(actualMin,expectedMin, "Error in Task1 : Expected '" + expectedMin + "', but found '" +actualMin+ "'");
+
+
+        //>>>>>>>>>>>>>>>>>>>>>>>>>Костль для фэйл теста**************************
+        //int expTempMin = 1;
+        //assertEquals(actualMin, expTempMin, "Error in Task1 : Expected minimal number '" + expTempMin + "', but found '" +actualMin+ "'");
+        //<<<<<<<<<<<<<<<<<<<<<<<<<Костль для фэйл теста**************************
+        System.out.println("Test passed");
     }
+
+    @DataProvider
+    public Object[][] getIntegerData() {
+        return new Object[][]{
+                {"1,2,3",   1},
+                {"3,2,1",   1},
+                {"-1,0,1", -1},
+                {"1,1,2",   1},
+                {"2,2,1",   1},
+                {"1,1,1",   1}
+        };
+    }
+
 }
